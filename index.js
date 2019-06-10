6780960f5bab66305ad92dd3cf4292c35d697c5f
Mario Camacho <mario.camacho@lime-energy.com>
2019-06-09 23:00:41 -0400

    Adding delete endpoint

--- index.js
+++ index.js
@@ -12,24 +12,44 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/articles', async (req, res) => {
-    const query = { deleted_at: undefined };
-    const { title, authors } = req.query;
+    try {
+        const query = { deleted_at: undefined };
+        const { title, authors } = req.query;
 
-    if (title) query.title = { $regex: title, $options: 'i' };
+        if (title) query.title = { $regex: title, $options: 'i' };
 
-    const response = await Article.find(query);
-    res.send(response);
+        const response = await Article.find(query);
+        res.send(response);
+    } catch(error) {
+        res.status(500).end();
+    }
 });
 
 app.post('/articles', async (req, res) => {
-    console.log(req);
-    console.log(req.body);
-    var article = new Article({
-        _id: uuid(),
-        title: req.body.title
-    });
-    const response = await article.save();
-    res.send(response);
+    try {
+        const now = new Date();
+        var article = new Article({
+            _id: uuid(),
+            title: req.body.title,
+            created_at: now,
+            updated_at:now,
+        });
+        const response = await article.save();
+        res.send(response);
+    } catch(error) {
+        res.status(500).end();
+    }
+});
+
+app.delete('/articles/:id', async (req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    try {
+        await Article.deleteOne({ "_id": id });
+        res.status(204).end();
+    } catch(error) {
+        res.status(500).end();
+    }
 });
 
 app.listen(PORT, () => console.log(`Articles app server is running on port ${PORT}`));
