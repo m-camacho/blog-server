b3edc3a742d0221dd799110c23360aba7d22c269
Mario Camacho <mario.camacho@lime-energy.com>
2019-06-11 16:26:12 -0400

    Adding endpoint to get a single article

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ import uuid from 'uuid';
 import { isNullOrUndefined } from './utils';
 import Article from './models/article';
 
-const PORT = 3000;
-mongoose.connect('mongodb://localhost/store');
+const PORT = 3001;
+mongoose.connect('mongodb://localhost/store', { useFindAndModify: false });
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/articles', async (req, res) => {
     try {
@@ -21,6 +28,25 @@ app.get('/articles', async (req, res) => {
         if (title) query.title = { $regex: title, $options: 'i' };
 
         const response = await Article.find(query);
+        res.send(response);
+    } catch(error) {
+        console.log(error);
+        res.status(500).end();
+    }
+});
+
+app.get('/articles/:id', async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        res.status(400).send('id is required for this operation');
+        return;
+    }
+    try {
+        const query = {
+            _id: id,
+            deleted_at: undefined 
+        };
+        const response = await Article.findOne(query);
         res.send(response);
     } catch(error) {
         console.log(error);
@@ -63,9 +89,9 @@ app.put('/articles/:id', async (req, res) => {
     try {
         const now = new Date();
         const updateFields = { updated_at:now };
-        if (!isNullOrUndefined(title)) article.title = title;
-        if (!isNullOrUndefined(short_description)) article.short_description = short_description;
-        if (!isNullOrUndefined(long_description)) article.long_description = long_description;
+        if (!isNullOrUndefined(title)) updateFields.title = title;
+        if (!isNullOrUndefined(short_description)) updateFields.short_description = short_description;
+        if (!isNullOrUndefined(long_description)) updateFields.long_description = long_description;
         
         const response = await Article.findOneAndUpdate({ "_id": id }, updateFields, { new: true });
         res.send(response);
